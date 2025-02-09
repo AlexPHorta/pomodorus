cd30cc27f1f6d13e5140012f20a9b13fba75769a
AlexHorta <buey.horta@gmail.com>
2025-02-09 15:17:58 -0300

    Primeiros testes.

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,5 +1,14 @@
 
 const formatTime = (minutes, seconds) => {
+    try {
+      let minutes_ok = minutes >= 0 && minutes <= 99;
+      let seconds_ok = seconds >=0 && seconds <= 59;
+      if (!(minutes_ok && seconds_ok)) {
+        throw new Error("Bad parameter. Out of range."); 
+      }
+    } catch (err) {
+        alert( "Range Error: " + err.message );
+    }
     return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 };
 
